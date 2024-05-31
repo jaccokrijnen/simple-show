5c3283e7904f81c384f9eb711419c57a41d92f42
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2024-05-31 11:49:26 +0200

    Rename to ShowPrefix

--- src/Text/SimpleShow.hs
+++ src/Text/SimpleShow.hs
@@ -57,67 +57,67 @@ instance (GIsApplied f, GIsApplied g) => GIsApplied (f :+: g) where
 
 
 
-class GShow a where
+class ShowPrefix a where
   gshow :: a -> String
 
-  default gshow :: (Generic a, GShow' (Rep a)) => a -> String
+  default gshow :: (Generic a, ShowPrefix' (Rep a)) => a -> String
   gshow x = gshow' (from x)
 
-class GShow' f where
+class ShowPrefix' f where
   gshow' :: f a -> String
 
-instance GShow' U1 where
+instance ShowPrefix' U1 where
   gshow' U1 = ""
 
-instance (GShow' f, GShow' g) => GShow' (f :*: g) where
+instance (ShowPrefix' f, ShowPrefix' g) => ShowPrefix' (f :*: g) where
   gshow' (x :*: y) = gshow' x ++ " " ++ gshow' y
 
-instance (GShow' f, GShow' g) => GShow' (f :+: g) where
+instance (ShowPrefix' f, ShowPrefix' g) => ShowPrefix' (f :+: g) where
   gshow' (L1 x) = gshow' x
   gshow' (R1 x) = gshow' x
 
-instance (GShow' f) => GShow' (M1 D c f) where
+instance (ShowPrefix' f) => ShowPrefix' (M1 D c f) where
   gshow' (M1 x) = gshow' x
 
-instance (GShow' f, Constructor c, GIsApplied f) => GShow' (M1 C c f) where
+instance (ShowPrefix' f, Constructor c, GIsApplied f) => ShowPrefix' (M1 C c f) where
   gshow' m@(M1 x)
     | gIsApplied x = parens True $ conName m ++ " " ++ gshow' x
     | otherwise = conName m
 
-instance (GShow' f, Selector s) => GShow' (M1 S s f) where
+instance (ShowPrefix' f, Selector s) => ShowPrefix' (M1 S s f) where
   -- Ignore selector names
   gshow' (M1 x) = gshow' x
 
-instance (GShow a) => GShow' (K1 i a) where
+instance (ShowPrefix a) => ShowPrefix' (K1 i a) where
   gshow' (K1 x) = gshow x
 
-instance GShow Char where
+instance ShowPrefix Char where
   gshow = show
 
-instance GShow Int where
+instance ShowPrefix Int where
   gshow = show
 
-instance GShow Bool where
+instance ShowPrefix Bool where
   gshow = show
 
-instance {-# OVERLAPPING #-} GShow String where
+instance {-# OVERLAPPING #-} ShowPrefix String where
   gshow = show
 
-instance GShow Double where
+instance ShowPrefix Double where
   gshow = show
 
-instance (GShow a, GShow b) => GShow (a, b) where
+instance (ShowPrefix a, ShowPrefix b) => ShowPrefix (a, b) where
   gshow (a, b) = "(" ++ gshow a ++ ", " ++ gshow b ++ ")"
 
-instance GShow a => GShow [a]
+instance ShowPrefix a => ShowPrefix [a]
 
 data Person = Person { name :: String, age :: Int, ints :: [Int]}
   deriving (Generic)
 
-instance GShow Person
+instance ShowPrefix Person
 
 data Shape = Circle Double | ShapePerson Person
   deriving (Generic)
 
-instance GShow Shape
+instance ShowPrefix Shape
 
